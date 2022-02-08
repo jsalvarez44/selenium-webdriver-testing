84ae8e0109f8a607a3312a54ea5bd85859bdd240
jsalvarez44 <jsalvarez4@espe.edu.ec>
2022-02-08 17:17:07 -0500

    Upload selenium config

---
+++ src/main/java/com/maven/webtesting/App.java
@@ -0,0 +1,7 @@
+package com.maven.webtesting;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/maven/webtesting/AppTest.java
@@ -0,0 +1,26 @@
+package com.maven.webtesting;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AppTest {
+    private WebDriver driver;
+
+    @Before
+    public void configureDriver() {
+        System.setProperty("webdriver.chrome.driver",
+                "C:/Users/sebask8er/Desktop/Selenium WebTesting/selenium/driver/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+    }
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
